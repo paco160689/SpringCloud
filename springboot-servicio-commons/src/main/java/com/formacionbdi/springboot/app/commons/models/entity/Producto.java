8c8b7a7763b5d1b0e7554c90b2fa9cb2d7e62eb6
paco16 <paco160689@gmail.com>
2020-03-22 19:48:23 +0100

    Se crea proyecto commons y se cambian los imports

--- springboot-servicio-commons/src/main/java/com/formacionbdi/springboot/app/commons/models/entity/Producto.java
+++ springboot-servicio-commons/src/main/java/com/formacionbdi/springboot/app/commons/models/entity/Producto.java
@@ -1,4 +1,4 @@
-package com.formacionbdi.springboot.app.productos.models.entity;
+package com.formacionbdi.springboot.app.commons.models.entity;
 
 import java.io.Serializable;
 import java.util.Date;
